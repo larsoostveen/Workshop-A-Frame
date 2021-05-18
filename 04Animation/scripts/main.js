e25d16b6d803ad6ee417a0078aacd9fcfa14c537
larsoostveen <oostveen.lars@gmail.com>
2021-05-18 14:25:42 +0200

    Update main.js

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -5,7 +5,9 @@
  var myOtherBox = document.getElementById('myOtherBox');
 
  function spin(){
- 	myOtherBox.object3D.rotation.x += rotationSpeed - 2;
+ 	myOtherBox.object3D.rotation.x += rotationSpeed;
+    myOtherBox.object3D.rotation.z += rotationSpeed/2;
+
  	console.log(myOtherBox.object3D.rotation.x);
  }
 
